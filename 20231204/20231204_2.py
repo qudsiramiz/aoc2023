020664badbc7a45ba35a50762949fc16a182a082
qudsiramiz <qudsiramiz@gmail.com>
2023-12-04 08:54:40 -0500

    day 4 soluttions

--- 20231204/20231204_2.py
+++ 20231204/20231204_2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Read the input file
 with open("../data/20231204_input.txt") as f:
     content = f.readlines()
@@ -7,6 +9,7 @@ content = [x.strip() for x in content]
 
 # Part 1
 points = []
+
 for line in content:
     w_numbers = line.split(":")[1].split("|")[0].split(" ")
     # Get rid of empty strings
@@ -21,9 +24,19 @@ for line in content:
     for number in m_numbers:
         if number in w_numbers:
             count += 1
-    if count > 0:
-        points.append(2 ** (count - 1))
+    points.append(count)
 
-# Add up all the winning values
-print(sum(points))
 
+# Number of cards
+number_cards = np.ones(len(points))
+
+for i, (point_value, number_cards_val) in enumerate(zip(points, number_cards)):
+    if point_value == 0:
+        continue
+    else:
+        for k in range(int(number_cards_val)):
+            for j in range(point_value):
+                number_cards[int(i + j + 1)] += 1
+
+# Add up all the number_cards
+print(sum(number_cards))
